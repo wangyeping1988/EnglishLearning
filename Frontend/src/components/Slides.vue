864897bb949643d1b99a2a8e0c9b417f2f3a3e8a
wangyeping1988 <wangyeping1988@outlook.com>
2018-09-19 00:28:09 +0800

    slide.vue

---
+++ Frontend/src/components/Slides.vue
@@ -0,0 +1,45 @@
+<template>
+  <!-- <el-carousel indicator-position="outside" :height="bannerHeight + 'px'"> -->
+  <el-carousel indicator-position="outside">
+    <el-carousel-item v-for="(item,index) in 4" :key=index>
+      <img src="../assets/images/banner1.jpg" v-if="index == 0" class="bannerImg" />
+      <img src="../assets/images/banner2.jpg" v-if="index == 1" class="bannerImg" />
+      <img src="../assets/images/banner3.jpg" v-if="index == 2" class="bannerImg" />
+      <img src="../assets/images/banner4.jpg" v-if="index == 3" class="bannerImg" />
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<script>
+export default {
+  name: 'Slides',
+  methods: {
+    // setSize: function () {
+    //   this.bannerHeight = 740 / 2560 * this.screenWidth
+    //   if (this.bannerHeight > 740) this.bannerHeight = 740
+    //   if (this.bannerHeight < 360) this.bannerHeight = 360
+    // }
+  },
+  mounted: function () {
+    // this.setSize()
+    // const that = this
+    // window.addEventListener(
+    //   'resize',
+    //   function () {
+    //     that.screenWidth = $(window).width()
+    //     that.setSize()
+    //   },
+    //   false
+    // )
+  }
+}
+</script>
+
+<style scoped>
+.bannerImg {
+  width: 100%;
+  height: 100%;
+  min-height: 450px;
+  min-width: 800px;
+}
+</style>
--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Slides from '@/components/Slides'
 
 Vue.use(Router)
 
@@ -8,6 +9,11 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'Slides',
+      component: Slides
+    },
+    {
+      path: '/HelloWorld',
       name: 'HelloWorld',
       component: HelloWorld
     }
